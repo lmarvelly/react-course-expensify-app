37e0f10ce5076ccae8c709185ad2f94207eee784
Luke Marvelly <lmarvelly@hotmail.com>
2021-02-16 10:00:20 +0000

    Added more functionality to ExpenseForm component. It will now submit a new expense to the array or expenses. Added code to AppExpensePage component to handle the ExpenseForm correctly

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import moment from 'moment';
+import { SingleDatePicker } from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
+
+const now = moment();
+console.log(now.format('MMM Do, YYYY'));
 
 export default class ExpenseForm extends React.Component
 {	
@@ -7,7 +13,10 @@ export default class ExpenseForm extends React.Component
 	{
 		description: '',
 		amount: '',
-		note: ''
+		note: '',
+		createdAt: moment(),
+		calenderFocused: false,
+		error: ''
 	};
 	onDescriptionChange = (e) =>
 	{
@@ -18,21 +27,54 @@ export default class ExpenseForm extends React.Component
 	{
 		const amount = e.target.value;
 
-		if (amount.match(/^\d*(\.\d{0,2})?$/))
+		if (!amount || amount.match(/^\d{1,}(\.\d{0,2})?$/))
 		{
 			this.setState(() => ({ amount }));
 		}
 	}
+	onDateChange = (createdAt) =>
+	{
+		if (createdAt) // If statement is needed to prevent user from deleting date
+		{
+			this.setState(() => ({ createdAt }));
+		}
+	};
+	onFocusChange = ({ focused }) =>
+	{
+		this.setState(() => ({ calenderFocused: focused }));
+	};
 	onNoteChange = (e) =>
 	{
 		const note = e.target.value;
 		this.setState(() => ({ note }));
 	}
+	onSubmit = (e) =>
+	{
+		e.preventDefault(); // prevent full page reload
+
+		if (!this.state.description || !this.state.amount)
+		{
+			this.setState(() => ({ error: 'Please provide description and amount.' }));
+		}
+		else
+		{
+			this.setState(() => ({ error: ''}));
+			// onSubmit gets passed down by the AppExpensePage
+			this.props.onSubmit(
+			{
+				description: this.state.description,
+				amount: parseFloat(this.state.amount, 10) * 100,
+				createdAt: this.state.createdAt.valueOf(),
+				note: this.state.note
+			})
+		}
+	}
 	render()
 	{
 		return (
 			<div>
-				<form>
+				{ this.state.error && <p>{ this.state.error }</p>}
+				<form onSubmit={ this.onSubmit }>
 					<input
 						type='text'
 						placeholder='Description'
@@ -45,6 +87,15 @@ export default class ExpenseForm extends React.Component
 						placeholder='Amount'
 						value={ this.state.amount }
 						onChange={ this.onAmountChange }
+					/>
+					<SingleDatePicker 
+						date={ this.state.createdAt }
+						onDateChange={ this.onDateChange }
+						focused= { this.state.calenderFocused }
+						onFocusChange={ this.onFocusChange }
+						numberOfMonths={ 1 }
+						isOutsideRange={ () => false } // makes everysingle day in future and past available
+						displayFormat="DD/MM/YYYY" // UK date format
 					/>
 					<textarea
 						placeholder='Add a note for your expense (optional)'
