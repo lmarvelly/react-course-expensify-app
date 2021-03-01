a13686b104e905071f289e42273ab71229fce1b9
Luke Marvelly <lmarvelly@hotmail.com>
2021-03-01 17:03:57 +0000

    Setup formatting for amount and createAt

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';  // You can change the currency http://numeraljs.com/#locales
 
 // If we want to see note then we add it as an argument and then you can add it to the paragraph: { note }
 export const ExpenseListItem = ({ id, description, amount, createdAt }) => 
@@ -7,7 +9,11 @@ export const ExpenseListItem = ({ id, description, amount, createdAt }) =>
 	<div>
 		<Link to={ `/edit/${id}` }><h3>{ description } </h3></Link>
 
-		<p>{ amount } - { createdAt }</p>
+		<p>
+			{ numeral( amount / 100 ).format('$0,0.00') }
+			- 
+			{ moment( createdAt ).format('MMMM Do, YYYY') }
+		</p>
 	</div>
 );	
 
