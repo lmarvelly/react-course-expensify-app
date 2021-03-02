35664f3e45620388c463cf3577f2d8e5c092c896
Luke Marvelly <lmarvelly@hotmail.com>
2021-03-02 13:35:12 +0000

    Updated expenseSummary to support test cases and finished test cases

--- src/tests/components/ExpenseSummary.test.js
+++ src/tests/components/ExpenseSummary.test.js
@@ -1,9 +1,23 @@
 import React from 'react';
-import {ExpenseSummary} from '../../components/ExpenseSummary';
+import { ExpenseSummary } from '../../components/ExpenseSummary';
 import expenses from '../fixtures/expenses';
 import { shallow } from 'enzyme';
 
+// test('should generate an expense totaling £46.95', () =>
+// {
+// 	const wrapper = shallow(<ExpenseSummary expenses={ [] } />);
+// 	expect(wrapper).toMatchSnapshot();
+// });
+
 test('should generate an expense totaling $195.00', () =>
 {
-	const wrapper = shallow(<ExpenseSummary expenses={ expenses[1] } />);
+	// Shallow render JSX
+	const wrapper = shallow(<ExpenseSummary expenseCount={ 1 } expensesTotal={ 235 } />);
+	expect(wrapper).toMatchSnapshot();
+});
+
+test('should generate an expense totaling £46.95', () =>
+{
+	const wrapper = shallow(<ExpenseSummary expenseCount={ 5 } expensesTotal={ 2523 } />);
+	expect(wrapper).toMatchSnapshot();
 });
