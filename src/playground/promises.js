b19768cb35708b0317693c7b43e2bc19b46f6ec1
Luke Marvelly <lmarvelly@hotmail.com>
2021-03-07 15:55:57 +0000

    updating expensers to work with firebase

--- src/playground/promises.js
+++ src/playground/promises.js
@@ -2,13 +2,14 @@ const promise = new Promise((resolve, reject) =>
 {
 	setTimeout(() => 
 	{
-		// resolve(
-		// {
-		// 	name: 'Luke Marvelly',
-		// 	age: 32
-		// });
-		reject('Something went wrong');
-		resolve('This is my other resolved data'); // this will be ignored
+		resolve(
+		{
+			name: 'Luke Marvelly',
+			age: 32
+		});
+		// When we reject from a promise, none of the success cases get called (in this case the then() method)
+		// reject('Something went wrong');
+		// resolve('This is my other resolved data'); // this will be ignored
 	}, 2000);
 });
 
@@ -16,9 +17,17 @@ console.log('before');
 
 // then() registers a callback. This will fire if and when 
 // the promise resolves. Second argument is treated as catch error like catch() if needed
+// You can have a second then
 promise.then((data) => 
 {
 	console.log('1', data);
+
+	// The next then() statement has access the the returned value below
+	return 'Some data'; 
+})
+.then(() =>
+{
+	console.log('2', 'this runs after first then()')
 })
 .catch((error) => 
 {
--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,5 +1,9 @@
-import { addExpense, editExpense, removeExpense } from '../../actions/expensers';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { startAddExpense, addExpense, editExpense, removeExpense } from '../../actions/expensers';
 import expenses from '../fixtures/expenses';
+
+const createMockStore = configureMockStore([thunk]);
 
 test('should setup remove expense action object', () => 
 {
@@ -33,6 +37,37 @@ test('should setup add expense action object with provided values', () =>
 		type: 'ADD_EXPENSE',
 		expense: expenses[2]
 	});
+});
+
+/**
+ * Using done parameter forces jest to wait till a specific 
+ * point in time.
+ * 
+ * Needs to be used with testing firebase because firebase 
+ * is asynchronous and done makes the test case wait until 
+ * firebase has finished doing it's thing.  
+ */
+test('should add expense to database and store', (done) =>
+{
+	const store = createMockStore({});
+	const expenseData = 
+	{
+		description: 'Mouse',
+		amount: 3000,
+		note: 'This one is better', 
+		createdAt: 1000
+	};
+
+	store.dispatch(startAddExpense(expenseData)).then(() =>
+	{
+		expect(1).toBe(1);
+		done();
+	});
+});
+
+test('should add expense with defaults to database and store', () =>
+{
+
 });
 
 // test('should setup add expense action object with default values', () =>
