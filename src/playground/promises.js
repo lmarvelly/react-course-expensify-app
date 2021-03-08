1c03b759e192b9472a4737a548a4e32adac6e746
Luke Marvelly <lmarvelly@hotmail.com>
2021-03-08 11:04:16 +0000

    Created test case for checking that an expense was added to database

--- src/playground/promises.js
+++ src/playground/promises.js
@@ -13,30 +13,61 @@ const promise = new Promise((resolve, reject) =>
 	}, 2000);
 });
 
+// console.log('before');
+
+// // then() registers a callback. This will fire if and when 
+// // the promise resolves. Second argument is treated as catch error like catch() if needed
+// // You can have a second then
+// promise.then((data) => 
+// {
+// 	console.log('1', data);
+
+// 	// The next then() statement has access the the returned value below
+// 	return 'Some data'; 
+// })
+// .then((str) =>
+// {
+// 	console.log('2', 'this runs after first then()')
+// })
+// .catch((error) => 
+// {
+// 	console.log('Error: ', error);
+// });
+
+// // promise.then((data) => 
+// // {
+// // 	console.log('2', data);
+// // });
+
+// console.log('after');
+
+
+
+// ###################################
+// ###### Return second promise ######
+// ###################################
 console.log('before');
 
-// then() registers a callback. This will fire if and when 
-// the promise resolves. Second argument is treated as catch error like catch() if needed
-// You can have a second then
 promise.then((data) => 
 {
 	console.log('1', data);
 
-	// The next then() statement has access the the returned value below
-	return 'Some data'; 
+	return new Promise((resolve, reject) =>
+	{
+		setTimeout(() => 
+		{
+			resolve('This is my other promise');
+		}, 2000);
+	});
 })
-.then(() =>
+.then((str) =>
 {
-	console.log('2', 'this runs after first then()')
+	console.log('does this run?', str)
 })
 .catch((error) => 
 {
 	console.log('Error: ', error);
 });
 
-// promise.then((data) => 
-// {
-// 	console.log('2', data);
-// });
-
 console.log('after');
+
--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -2,6 +2,7 @@ import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import { startAddExpense, addExpense, editExpense, removeExpense } from '../../actions/expensers';
 import expenses from '../fixtures/expenses';
+import database from '../../firebase/firebase';
 
 const createMockStore = configureMockStore([thunk]);
 
@@ -60,7 +61,24 @@ test('should add expense to database and store', (done) =>
 
 	store.dispatch(startAddExpense(expenseData)).then(() =>
 	{
-		expect(1).toBe(1);
+		const actions = store.getActions();
+
+		expect(actions[0]).toEqual(
+		{
+			type: 'ADD_EXPENSE',
+			expense: 
+			{
+				id: expect.any(String),
+				...expenseData
+			}
+		});
+
+		return database.ref(`expenses/${actions[0].expense.id}`)
+			.once('value');
+	})
+	.then((snapshot) => 
+	{
+		expect(snapshot.val()).toEqual(expenseData);
 		done();
 	});
 });
