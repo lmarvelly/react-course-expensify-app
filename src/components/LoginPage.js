829cf8c10201878e59b2bd5c865f258467634257
Luke Marvelly <lmarvelly@hotmail.com>
2021-03-13 12:05:30 +0000

    Created Login page and snapshot test for said page

---
+++ src/components/LoginPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const LoginPage = () =>
+(
+	<div>
+		<form>
+			<input type='text' placeholder='Username' />
+			<input type='password' placeholder='Password' />
+		</form>
+	</div>
+);
+
+export default LoginPage;
