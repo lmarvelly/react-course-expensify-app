f9fb97bc9a207cc7a51be00e83d01914c193d1b8
Luke Marvelly <lmarvelly@hotmail.com>
2021-11-28 13:42:52 +0000

    Added Login message

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -8,6 +8,7 @@ export const LoginPage = ({ startLogin }) =>
 		<div className="box-layout__box">
 			<h1 className="box-layout__title">Expensify</h1>
 			<p>It's time to get your expenses under control.</p>
+			<p className='bold'>Please Login to use App</p>
 			<button 
 				onClick={ startLogin } 
 				className="button"
